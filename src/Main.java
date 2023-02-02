e6da5134cc3e9e11d3fcd6c696447014109840cc
Erica Castro <ericastro0615@gmail.com>
2023-02-02 17:30:40 -0300

    Erro resuelto

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
-    //instaciar DE OBJETO tipo servicio
-   VendedorService vs = new VendedorService();
-    //instaciacion donde se aloja el retorno de la clase servcio, vinculacion y retorno del objeto de las clases
-   Vendedor v1 = vs.altaVendedor();
-    //llamada de metodos. NO SE PEUDE VINCULAR!!
-    //vs.vacaciones(v1);
-
-
+    public static void main(String[] args) {
+        //instaciar DE OBJETO tipo servicio
+        VendedorService vs = new VendedorService();
+        //instaciacion donde se aloja el retorno de la clase servcio, vinculacion y retorno del objeto de las clases
+        Vendedor v1 = vs.altaVendedor();
+        //llamada de metodos. NO SE PEUDE VINCULAR!!
+        vs.vacaciones(v1);
+        //vs.sueldoMensual();
+    }
     }
